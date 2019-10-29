69526548cae4640aa00b6c939019ad68ef7946d3
José Paz <jpaz17@hotmail.com>
2019-10-29 13:13:09 -0300

    Agregado archivo de configuración para conexión a base de datos

--- src/main/java/com/example/demo/DB/ConnectionFactory.java
+++ src/main/java/com/example/demo/DB/ConnectionFactory.java
@@ -8,17 +8,11 @@ package com.example.demo.DB;
 import java.sql.*;
 
 public class ConnectionFactory {
-    private final static String DBDRIVER = "jdbc:postgresql://";
-    private final static String DBHOST = "ec2-107-21-126-201.compute-1.amazonaws.com";
-    private final static String DBPORT = "5432";
-    private final static String DBNAME = "d2tes8ukbdj1u4";
-    private final static String DBUSER = "byvblkdrkfblrj";
-    private final static String DBPASS = "8dfa6f47a45a64c140a82d5aa2dcfb3705f08c1bc4a6b520d31cdb31cde992ee";
-    
     public static Connection getConnection(){
-        String DBURL = DBDRIVER + DBHOST + ":" + DBPORT + "/" + DBNAME;
+        String[] all = ManejadorArchivosGenerico.leerArchivo("./src/configuration.txt", true);
+        String DBURL = all[0] + all[1] + ":" + all[2] + "/" + all[3];
         try {
-            return DriverManager.getConnection(DBURL, DBUSER, DBPASS);
+            return DriverManager.getConnection(DBURL, all[4], all[5]);
         } catch(SQLException sqle) {
             throw new RuntimeException("Error conectando a la base de datos", sqle);
         }
@@ -28,9 +22,5 @@ public class ConnectionFactory {
         try {
             con.close();
         } catch (SQLException sqle) {}
-    }
-    
-    public static String getDBHOST() {
-        return DBHOST;
     }
 }
